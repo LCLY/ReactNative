e5403c65f27ae37a26fb753673cb2d2ae6fad683
lchoo <lchoo1996@gmail.com>
2019-10-22 02:21:16 -0700

    refactor: bind id to child func

--- rn-app/components/GoalItem.js
+++ rn-app/components/GoalItem.js
@@ -2,7 +2,10 @@ import React from "react";
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 const GoalItem = props => {
     return (
-        <TouchableOpacity activeOpacity={0.8} onPress={props.onDelete}>
+        <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={props.onDelete.bind(this, props.id)}
+        >
             <View style={styles.listItem}>
                 <Text>{props.title}</Text>
             </View>
