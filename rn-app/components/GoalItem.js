738620b6951cdd21b4f4158d1974cdff7d6b97e3
lchoo <lchoo1996@gmail.com>
2019-10-22 01:59:52 -0700

    refactor: adjust opacity level

--- rn-app/components/GoalItem.js
+++ rn-app/components/GoalItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 const GoalItem = props => {
     return (
-        <TouchableOpacity onPress={props.onDelete}>
+        <TouchableOpacity activeOpacity={0.8} onPress={props.onDelete}>
             <View style={styles.listItem}>
                 <Text>{props.title}</Text>
             </View>
